3f52e43694ab0f71b026974064d88f6e2cd4baea
Lenka Pavlikova <pavlikovalenk@seznam.cz>
2023-04-24 12:24:20 +0200

    Add recursive_nth_fibo

--- fibo.py
+++ fibo.py
@@ -1,7 +1,16 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
 
-def main():
-    pass
+print(recursive_nth_fibo(7))
 
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     pass
+#
+#
+# if __name__ == "__main__":
+#     main()
